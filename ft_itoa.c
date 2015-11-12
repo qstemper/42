421d13b8194298784299517397d704bfaf66f1f1
Quentin Stempert <qstemper@Quentins-Mac-mini.local>
2015-11-12 05:00:42 -0800

    ft_itoa.c

---
+++ ft_itoa.c
@@ -0,0 +1,45 @@
+#include "libft.h"
+
+int	ft_nblen(int c)
+{
+	int	i;
+
+	i = 0;
+	if (c < 0)
+	{
+		c = -c;
+		i++;
+	}	
+	while (c >= 1)
+	{
+		c = c / 10;
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	int	i;
+
+	i = 0;
+	if((str = (char *)malloc(sizeof(char) * ft_nblen(n)  + 1)) != NULL)
+	{
+		if (n < 0)
+		{
+			str[i] = '-';
+			i++;
+			n = -n;
+		}
+		while (n >= 1)
+		{
+			str[i] = n % 10 + 48;
+			n = n / 10;
+			i++;
+		}
+		str[i] = '\0';
+		return (str);
+	}
+	return (NULL);
+}
--- libft.h
+++ libft.h
@@ -65,7 +65,7 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtim(char const *s);
 char	**ft_strsplit(char const *s, char c);
-
+char	*ft_itoa(int n);
 
 
 #endif
