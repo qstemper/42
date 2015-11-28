45c4a05bb0eb7369de0a91ad8c629b168fa72ef0
Quentin STEMPERT <qstemper@e2r9p10.42.fr>
2015-11-28 16:47:15 +0100

    maj_gnl project Normed and Functional

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,141 +1,117 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: qstemper <qstemper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/11/27 10:13:34 by qstemper          #+#    #+#             */
-/*   Updated: 2015/11/27 19:26:58 by qstemper         ###   ########.fr       */
+/*   Created: 2015/11/28 12:14:21 by qstemper          #+#    #+#             */
+/*   Updated: 2015/11/28 16:45:40 by qstemper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-//testbranch
 
 #include "get_next_line.h"
 
-static int	ft_checkstore(char *storage)
+static int		ft_checkstatic(char **line, char *storage_buffer)
 {
 	char		*find;
-	char		**line;
 
-	find = ft_strchr(storage, '\n');
+	find = ft_strchr(storage_buffer, '\n');
 	if (find == NULL)
-		return (0);
+		return (-1);
 	*find = 0;
-	*line = ft_strdup(storage);
-	ft_strcpy(storage, find + 1);
+	*line = ft_strdup(storage_buffer);
+	ft_strcpy(storage_buffer, find + 1);
 	return (1);
 }
 
-static int	ft_readton(char *buffer)
+static void		ft_lsttoline(char **line, t_list **lst)
 {
-	int		storage_read;
-	char	*find;
-
-	storage_read = read(fd, buffer, BUFF_SIZE);
-	if (storage_read == -1)
-		return (storage_read);
-	buffer[storage_read] = '\0';
-	ft_bzero((void *)(buffer + storage_read), (BUFF_SIZE - storage_read));
-	return (storage_read);
-}
-
-static void		ft_lstcons()
-{
-	
-}
-
-int			get_next_line(int const fd, char **line)
-{
-	static char	storage_buffer[BUFF_SIZE + 1];
-	char		buffer[BUFF_SIZE + 1];
-	char		*find;
-	t_list		*listbuff;
-	t_list		*elem;
 	t_list		*tmp;
-	int			storage_read;
-	int			flag;
-	int			sum;
 
+	while (*lst)
+	{
+		tmp = (*lst)->next;
+		ft_strcat(*line, (char *)(*lst)->content);
+		ft_lstdelone(lst, NULL);
+		*lst = tmp;
+	}
+}
+
+static int		ft_linecons(char **line, \
+				char *storage_buffer, t_list **lst, char *buffer)
+{
+	int			len;
+	t_list		*tmp;
+	char		*find;
+
+	len = ft_strlen(storage_buffer);
+	tmp = *lst;
+	while (tmp)
+	{
+		len += tmp->content_size;
+		tmp = tmp->next;
+	}
+	find = ft_strchr(buffer, '\n');
+	if (find == NULL && *lst == NULL)
+		return (1);
+	else if (find)
+		*find = '\0';
+	len += ft_strlen(buffer);
+	if ((*line = ft_memalloc((len + 1) * sizeof(char))) == NULL)
+		return (-1);
+	ft_strcpy(*line, storage_buffer);
+	ft_lsttoline(line, lst);
+	ft_strcat(*line, buffer);
+	return (1);
+}
+
+static int		ft_readton(int fd, char *buffer, t_list **lst)
+{
+	char		*find;
+	int			check;
+	t_list		*elem;
+
+	find = NULL;
+	check = -1;
+	while (find == NULL && check != 0)
+	{
+		ft_bzero((void *)buffer, MY_SIZE);
+		check = read(fd, buffer, BUFF_SIZE);
+		if (check == -1)
+			return (-1);
+		buffer[check] = '\0';
+		find = ft_strchr(buffer, '\n');
+		if (find == NULL && check != 0)
+		{
+			if ((elem = ft_lstnew((void *)buffer, \
+							(check + 1) * sizeof(char))) == NULL)
+				return (-1);
+			ft_lstaddback(lst, elem);
+		}
+	}
+	if (check == 0)
+		return (0);
+	return (1);
+}
+
+int				get_next_line(int const fd, char **line)
+{
+	static char	storage_buffer[OPENED_FILE_LIMIT][MY_SIZE];
+	char		buffer[MY_SIZE];
+	t_list		*lst;
+	int			check;
+
+	*line = NULL;
 	if (BUFF_SIZE <= 0 || fd < 0 || line == NULL)
 		return (-1);
-	if (ft_checkstore(storage_buffer) == 1)
+	if (ft_checkstatic(line, storage_buffer[fd]) == 1)
 		return (1);
-	flag = 0;
-	listbuff = NULL;
-	sum = ft_strlen(storage_buffer);
-	storage_read = ft_readton(buffer);
-	while (flag != 1)
-	{
-		if (storage_read == -1)
-			return (storage_read);
-		find = ft_strchr(buffer, '\n');
-		if (find == NULL && storage_read != 0)
-		{
-			elem = ft_lstnew((void *)buffer, storage_read * sizeof(char));
-			if (elem == NULL)
-				return (-1);
-			ft_lstaddback(&listbuff, elem);
-			sum += storage_read;
-		}
-
-		else if (find != NULL)
-		{
-			flag = 1;
-			sum += (find - buffer + 1);
-			if ((*line = (char *)ft_memalloc(sizeof(char) * sum)) == NULL)
-				return (-1);
-			ft_strcat(*line, storage_buffer);
-			while (listbuff)
-			{
-				ft_strcat(*line, (char *)listbuff->content);
-				tmp = listbuff->next;
-				ft_lstdelone(&listbuff, NULL);
-				listbuff = tmp;
-			}
-			*find = 0;
-			ft_strcat(*line, buffer);
-			ft_strcpy(storage_buffer, find + 1);
-		}
-		else
-		{
-			flag = 1;
-			sum += (ft_strlen(buffer + 1));
-			if ((*line = (char *)ft_memalloc(sizeof(char) * sum)) == NULL)
-				return (-1);
-			ft_strcat(*line, storage_buffer);
-			while (listbuff)
-			{
-				ft_strcat(*line, (char *)listbuff->content);
-				tmp = listbuff->next;
-				ft_lstdelone(&listbuff, NULL);
-				listbuff = tmp;
-			}
-			ft_strcat(*line, buffer);
-			ft_strcpy(storage_buffer, buffer);
-		}
-	}
-	if (storage_read == 0)
-		return (0);
-	return (1);
+	lst = NULL;
+	if ((check = ft_readton(fd, buffer, &lst)) == -1)
+		return (-1);
+	if (ft_linecons(line, storage_buffer[fd], &lst, buffer) == -1)
+		return (-1);
+	ft_strcpy(storage_buffer[fd], buffer + ft_strlen(buffer) + 1);
+	return (*line ? 1 : 0);
 }
-
-/*#include <fcntl.h>
-int 	main()
-{
-	int fd;
-	char *line;
-
-	fd = open("test", O_RDONLY);
-	if (fd < 0)
-		return (1);
-	printf("[%d]\n", get_next_line(1, NULL));
-	printf("[%d]\n", get_next_line(42, &line));
-	while (get_next_line(fd, &line) > 0)
-	{
-		printf("[%s]\n", line);
-		free(line);
-	}
-	close(fd);
-	return (0);
-}*/
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: qstemper <qstemper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/27 09:13:45 by qstemper          #+#    #+#             */
-/*   Updated: 2015/11/27 16:21:37 by qstemper         ###   ########.fr       */
+/*   Updated: 2015/11/28 16:38:20 by qstemper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@
 # include "libft.h"
 
 # define BUFF_SIZE 42
+# define MY_SIZE BUFF_SIZE + 1
+# define OPENED_FILE_LIMIT 256
 
 int		get_next_line(int const fd, char **line);
 
