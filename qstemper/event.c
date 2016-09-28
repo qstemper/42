9c9e5783c88bb5aba2e821155681683bf531e9d7
Quentin STEMPERT <qstemper@e2r1p8.42.fr>
2016-09-28 17:57:48 +0200

    Master - Fractol

---
+++ qstemper/event.c
@@ -0,0 +1,120 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   event.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qstemper <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/28 07:45:31 by qstemper          #+#    #+#             */
+/*   Updated: 2016/09/28 10:08:54 by qstemper         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "frac.h"
+
+static void	key_scale(int keycode, t_env *e)
+{
+	float	zoom;
+
+	if (keycode == PAD_PLUS)
+	{
+		e->ptr->zoom_ratio *= 1.1;
+		zoom = e->ptr->zoom_ratio;
+		e->ptr->x1 += ((e->height / 2) / e->width / 2) / zoom * 10;
+		e->ptr->x2 += ((e->height / 2) / e->width / 2) / zoom * 10;
+		e->ptr->y1 += ((e->width / 2) / e->height / 2) / zoom * 10;
+		e->ptr->y2 += ((e->width / 2) / e->height / 2) / zoom * 10;
+	}
+	else if (keycode == PAD_MINUS)
+	{
+		e->ptr->zoom_ratio /= 1.1;
+		zoom = e->ptr->zoom_ratio;
+		e->ptr->x1 += ((e->height / 2) / e->width / 2) / zoom * 10;
+		e->ptr->x2 += ((e->height / 2) / e->width / 2) / zoom * 10;
+		e->ptr->y1 += ((e->width / 2) / e->height / 2) / zoom * 10;
+		e->ptr->y2 += ((e->width / 2) / e->height / 2) / zoom * 10;
+	}
+}
+
+static void	key_move(int keycode, t_env *e)
+{
+	if (keycode == UP_ARROW)
+	{
+		e->ptr->y1 += 10 / (e->ptr->zoom_ratio * 20);
+		e->ptr->y2 += 10 / (e->ptr->zoom_ratio * 20);
+	}
+	else if (keycode == DOWN_ARROW)
+	{
+		e->ptr->y1 -= 10 / (e->ptr->zoom_ratio * 20);
+		e->ptr->y2 -= 10 / (e->ptr->zoom_ratio * 20);
+	}
+	else if (keycode == LEFT_ARROW)
+	{
+		e->ptr->x1 += 10 / (e->ptr->zoom_ratio * 20);
+		e->ptr->x2 += 10 / (e->ptr->zoom_ratio * 20);
+	}
+	else if (keycode == RIGHT_ARROW)
+	{
+		e->ptr->x1 -= 10 / (e->ptr->zoom_ratio * 20);
+		e->ptr->x2 -= 10 / (e->ptr->zoom_ratio * 20);
+	}
+}
+
+int			mouse(int button, int x, int y, t_env *e)
+{
+	if (button == SCROLL_UP || button == LEFT_CLIC ||\
+			button == SCROLL_DOWN || button == RIGHT_CLIC)
+	{
+		e->ptr->x1 += (((float)x - e->height / 2) / e->width / 2)
+			/ e->ptr->zoom_ratio * 10;
+		e->ptr->x2 += (((float)x - e->height / 2) / e->width / 2)
+			/ e->ptr->zoom_ratio * 10;
+		e->ptr->y1 += (((float)y - e->width / 2) / e->height / 2)
+			/ e->ptr->zoom_ratio * 10;
+		e->ptr->y2 += (((float)y - e->width / 2) / e->height / 2)
+			/ e->ptr->zoom_ratio * 10;
+	}
+	if (button == SCROLL_UP || button == RIGHT_CLIC)
+		e->ptr->zoom_ratio *= 1.1;
+	if (button == SCROLL_DOWN || button == LEFT_CLIC)
+		e->ptr->zoom_ratio /= 1.1;
+	draw_img(e);
+	return (0);
+}
+
+int			motion(int x, int y, t_env *e)
+{
+	if (x >= 0 && y >= 0 && x <= e->width && y <= e->height && e->stop_motion)
+	{
+		e->ptr->c.r = (float)x / (float)e->width * 4 - 2;
+		e->ptr->c.im = (float)y / (float)e->height * 4 - 2;
+		draw_img(e);
+	}
+	return (0);
+}
+
+int			key(int keycode, t_env *e)
+{
+	if (keycode == ESC || keycode == QUIT)
+	{
+		mlx_destroy_window(e->mlx, e->win);
+		free(e->array);
+		free(e->col_array);
+		free(e);
+		exit(0);
+	}
+	else if (keycode == PAD_0)
+		e->stop_motion = !(e->stop_motion);
+	if (keycode == RELOAD)
+	{
+		init_frac(e);
+		frac_change(e->av[1], e);
+		draw_img(e);
+	}
+	key_color_change(keycode, e);
+	key_frac_change(keycode, e);
+	key_scale(keycode, e);
+	key_move(keycode, e);
+	draw_img(e);
+	return (0);
+}
---
+++ qstemper/tools.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tools.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qstemper <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/09/28 07:45:15 by qstemper          #+#    #+#             */
+/*   Updated: 2016/09/28 07:45:23 by qstemper         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "frac.h"
+
+int		rgb_creating(int r, int g, int b)
+{
+	return (((r & 0xff) << 16) + ((g & 0xff) << 8) + ((b & 0xff)));
+}
+
+void	frac_change(char *name, t_env *e)
+{
+	if (ft_strcmp("Mandelbrot", name) == 0)
+		e->ptr = &(e->array[0]);
+	else if (ft_strcmp("Julia", name) == 0)
+		e->ptr = &(e->array[1]);
+	else if (ft_strcmp("Menger", name) == 0)
+		e->ptr = &(e->array[2]);
+	else if (ft_strcmp("B_Ship", name) == 0)
+		e->ptr = &(e->array[3]);
+	else if (ft_strcmp("Mandelbar", name) == 0)
+		e->ptr = &(e->array[4]);
+}
+
+void	key_frac_change(int keycode, t_env *e)
+{
+	if (keycode == PAD_1)
+		frac_change("Mandelbrot", e);
+	else if (keycode == PAD_2)
+		frac_change("Julia", e);
+	else if (keycode == PAD_3)
+		frac_change("Menger", e);
+	else if (keycode == PAD_4)
+		frac_change("B_Ship", e);
+	else if (keycode == PAD_5)
+		frac_change("Mandelbar", e);
+}
+
+void	key_color_change(int keycode, t_env *e)
+{
+	if (keycode == BOARD_1)
+		change_color(0, e);
+	else if (keycode == BOARD_2)
+		change_color(1, e);
+	else if (keycode == BOARD_3)
+		change_color(2, e);
+	else if (keycode == BOARD_4)
+		change_color(3, e);
+}
